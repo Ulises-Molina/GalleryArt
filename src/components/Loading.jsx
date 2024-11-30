afd39eb01eaf2217cd72a836ef118c8d8caeeb95
Ulises Molina <uli.molina10@gmail.com>
2024-11-30 20:09:07 -0300

    Update Loading.jsx

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,8 +2,9 @@ import { CircularProgress } from "@mui/material"
 
 export const Loading = () => {
     return (
-        <div className="flex justify-center items-center h-screen w-screen">
+        <div className="flex flex-col justify-center items-center h-screen w-screen">
             <CircularProgress size="50px"/>
+            <p>Loading...</p>
         </div>
     )
 }
