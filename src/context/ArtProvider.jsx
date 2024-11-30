b9a9b56939d4ff3fb478357e57296beea4293de8
Ulises Molina <uli.molina10@gmail.com>
2024-11-30 20:01:08 -0300

    Update ArtProvider.jsx

--- src/context/ArtProvider.jsx
+++ src/context/ArtProvider.jsx
@@ -6,20 +6,21 @@ export const ArtProvider = ({children}) => {
 
     //Creo el estado loading para mostrar el spinner mientras se carga la informacion de la API
     const [loading, setLoading] = useState(true);
-
-
-
     const [art, setArt] = useState([])
 
     //Fetching de datos de la API y set del loading a false
-    const fetchingData = () => {
-        fetch('/db.json')
-        .then(res => res.json())
-        .then(data => setArt(data))
+    const fetchingData = async () => {
+    try {
+        const res = await fetch('/db.json');
+        const data = await res.json(); 
+        setArt(data);
+        setLoading(false);
+    } catch (error) {
+        console.error('Error fetching data:', error);  // Manejo de errores en caso de que algo falle
     }
+};
     useEffect(() => {
-        fetchingData()
-        console.log(loading)
+        fetchingData()  
     }, [])
 
 
