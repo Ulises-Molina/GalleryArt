ec49036760356ab1b868446dad8b519cb8e4de09
Ulises Molina <uli.molina10@gmail.com>
2024-12-06 10:12:32 -0300

    Agregue animaciones

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types"
 import { useNavigate } from "react-router-dom";
+import {motion} from "motion/react"
 
 export const Card = ({title,artist,imagen,id}) => {
 
@@ -20,11 +21,15 @@ const handleClick = () => {
 
 
     return (
-        <div className={`flex h-full w-full row-span-5 relative group hover:cursor-pointer min-w-64  p-3`}
+        <motion.div
+        initial={{ opacity: 0,translateY: 150 }}
+        whileInView={{ opacity: 1,translateY: 0,transition: { duration: .5 } }}
+        
+        className={`flex h-full w-full row-span-5 relative group hover:cursor-pointer min-w-64  p-3`}
         onClick={handleClick}>
                 <img src={imagen} alt="Work of art" className="w-full hover:scale-105 transition-transform duration-300 select-none"></img>
             <h2 className="font-roboto absolute bottom-12 left-1/2 transform -translate-x-1/2 text-white text-2xl whitespace-nowrap opacity-0 group-hover:opacity-90 transition-opacity duration-300">{title}</h2>
             <p className="font-roboto absolute left-1/2 transform -translate-x-1/2 bottom-5 whitespace-nowrap text-gray-300 opacity-0 group-hover:opacity-80 transition-opacity duration-300">{artist}</p>
-        </div>
+        </motion.div>
     )
 }
